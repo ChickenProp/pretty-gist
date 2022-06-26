b8bbd7d5f0f4d3725a3ed1c62b066c54bb6e0709
Philip Hazelden <philip.hazelden@gmail.com>
2022-06-26 12:30:25 +0100

    Allow parsing params from strings.
    Double now takes format strings (e.g. `"3.2f"`) instead of functions.

--- src/Gist.hs
+++ src/Gist.hs
@@ -4,7 +4,9 @@ module Gist
   , GPTextQuotes(..)
   ) where
 
+import           Data.Bifunctor                 ( first )
 import qualified Data.Char                     as Char
+import           Data.Functor                   ( (<&>) )
 import           Data.Kind                      ( Type )
 import qualified Data.Map.Strict               as Map
 import           Data.Map.Strict                ( Map )
@@ -14,6 +16,8 @@ import qualified Data.Set                      as Set
 import qualified Data.Text                     as Text
 import           Data.Text                      ( Text )
 import           Prettyprinter
+import qualified Text.Printf                   as Printf
+import           Text.Read                      ( readMaybe )
 
 class Monoid (GistParams a) => Gist a where
   type GistParams a :: Type
@@ -25,6 +29,12 @@ class Monoid (GistParams a) => Gist a where
   gist :: a -> Doc ann
   gist = gist' mempty
 
+  gistP :: [String] -> a -> Doc ann
+  gistP ps = gist' (mconcat $ map (parseParam @a) ps)
+
+  parseParam :: String -> GistParams a
+  parseParam _ = mempty
+
 instance Gist () where
   type GistParams () = ()
 
@@ -32,10 +42,49 @@ instance Gist Int where
   type GistParams Int = ()
 
 instance Gist Double where
-  type GistParams Double = Last (Double -> ShowS)
+  type GistParams Double = Last Printf.FieldFormat
   gist' = \case
     Last Nothing  -> pretty
-    Last (Just f) -> \d -> pretty (f d "")
+    Last (Just f) -> \d -> pretty $ Printf.formatRealFloat d f ""
+
+  parseParam = fromMaybe mempty . go
+   where
+    -- This is pretty awkward. Text.Printf doesn't expose any way to parse a
+    -- FieldFormat, so we do it ourselves. We might want our own version of
+    -- FieldFormat, to get a Show instance and to allow comma separation.
+    go = \case
+      [] -> Nothing
+      ('-' : s) ->
+        go s <&&> \f -> f { Printf.fmtAdjust = Just Printf.LeftAdjust }
+      ('+' : s) -> go s <&&> \f -> f { Printf.fmtSign = Just Printf.SignPlus }
+      (' ' : s) -> go s <&&> \f -> f { Printf.fmtSign = Just Printf.SignSpace }
+      ('0' : s) -> go s <&&> \f -> f { Printf.fmtAdjust = Just Printf.ZeroPad }
+      ('#' : s) -> go s <&&> \f -> f { Printf.fmtAlternate = True }
+      s         -> do
+        let isDigit         = (`elem` ("0123456789" :: String))
+            isFmtChar       = (`elem` ("fFgGeE" :: String))
+            (widthS, rest1) = span isDigit s
+        width <- if null widthS then Just Nothing else Just <$> readMaybe widthS
+        let (mPrecS, rest2) = case rest1 of
+              ('.' : s') -> first Just $ span isDigit s'
+              _          -> (Nothing, rest1)
+        prec <- case mPrecS of
+          Nothing    -> Just Nothing
+          Just ""    -> Just (Just 0)
+          Just precS -> Just <$> readMaybe precS
+        case rest2 of
+          (c : []) | isFmtChar c -> Just $ pure $ Printf.FieldFormat
+            { Printf.fmtWidth     = width
+            , Printf.fmtPrecision = prec
+            , Printf.fmtAdjust    = Nothing
+            , Printf.fmtSign      = Nothing
+            , Printf.fmtAlternate = False
+            , Printf.fmtModifiers = ""
+            , Printf.fmtChar      = c
+            }
+          _ -> Nothing
+
+    a <&&> f = fmap (fmap f) a
 
 data GPTextQuotes
   = GPTextQuotesAlways
@@ -53,6 +102,8 @@ instance Gist Text where
            (\c ->
              Char.isAlphaNum c
                || (      Char.generalCategory c
+                  -- In ascii this is just _ and -, but it includes some unicode
+                  -- characters too.
                   `elem` [Char.ConnectorPunctuation, Char.DashPunctuation]
                   )
            )
@@ -60,19 +111,32 @@ instance Gist Text where
         then pretty t
         else viaShow t
 
+  parseParam = \case
+    "QuotesAlways"    -> pure GPTextQuotesAlways
+    "QuotesNever"     -> pure GPTextQuotesNever
+    "QuotesSometimes" -> pure GPTextQuotesSometimes
+    _                 -> mempty
+
 instance (Gist a, Gist b) => Gist (a, b) where
   type GistParams (a, b) = (GistParams a, GistParams b)
   gist' (pa, pb) (a, b) = tupled [gist' pa a, gist' pb b]
+  parseParam s = (parseParam @a s, parseParam @b s)
 
 instance Gist a => Gist [a] where
   type GistParams [a] = (Last Int, GistParams a)
   gist' (Last mTake, p) l =
     let elems = case mTake of
           Nothing -> map (gist' p) l
-          Just n -> case splitAt n l of
-            (start, []) -> map (gist' p) start
-            (start, _:_) -> map (gist' p) start ++ ["..."]
-    in align $ list elems
+          Just n  -> case splitAt n l of
+            (start, []   ) -> map (gist' p) start
+            (start, _ : _) -> map (gist' p) start ++ ["..."]
+    in  align $ list elems
+
+  parseParam s = (mempty, parseParam @a s) <> case words s of
+    ["show-first", n] -> case readMaybe n of
+      Just n' -> (pure n', mempty)
+      Nothing -> mempty
+    _ -> mempty
 
 data GPMap = GPMap
   { gpMapKeys :: Last Bool
@@ -96,6 +160,11 @@ instance (Gist k, Gist v) => Gist (Map k v) where
       (if fromLast True showKeys then gist' pk k else "_")
         <> ": "
         <> (if fromLast True showVals then gist' pv v else "_")
+
+  parseParam s = (mempty, parseParam @k s, parseParam @v s) <> case s of
+    "hide-keys" -> (GPMap (pure False) mempty, mempty, mempty)
+    "hide-vals" -> (GPMap mempty (pure False), mempty, mempty)
+    _           -> mempty
 
 fromLast :: a -> Last a -> a
 fromLast def = \case
