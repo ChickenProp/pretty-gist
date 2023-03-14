9113e8c724a5ed2ae06eaceff93abbb72da7f860
Philip Hazelden <philip.hazelden@gmail.com>
2023-03-14 00:28:40 +0000

    Try a completely different approach.

--- src/Gist.hs
+++ src/Gist.hs
@@ -1,116 +1,213 @@
 module Gist
   ( Gist(..)
-  , GPMap(..)
-  , GPStrQuotes(..)
-  , runParamParser
+  , Configurable(..)
+  , config
+  , strConfig
+  , gist
+  , ConfMap(..)
+  , ConfStrQuotes(..)
   ) where
 
-import           Data.Bifunctor                 ( first
-                                                , second
-                                                )
+import           Data.Bifunctor                 ( first )
 import qualified Data.Char                     as Char
-import           Data.Functor                   ( (<&>) )
+import qualified Data.Dynamic                  as Dyn
 import           Data.Kind                      ( Type )
 import qualified Data.Map.Strict               as Map
 import           Data.Map.Strict                ( Map )
+import           Data.Maybe                     ( fromMaybe )
 import           Data.Monoid                    ( Last(..) )
+import           Data.String                    ( IsString )
 import qualified Data.Text                     as Text
 import           Data.Text                      ( Text )
-import           Data.Traversable               ( for )
 import           GHC.Stack                      ( HasCallStack )
 import           Prettyprinter
 import qualified Text.Printf                   as Printf
 import           Text.Read                      ( readMaybe )
+import           Type.Reflection                ( SomeTypeRep(..)
+                                                , TypeRep
+                                                , Typeable
+                                                , typeRep
+                                                )
 
-class (Monoid (GistParams a), Monoid (GistParamsList a)) => Gist a where
-  type GistParams a :: Type
-  type GistParams a = ()
+-- | A gist is a configurable pretty-print.
+class Configurable a => Gist a where
+  gist' :: Config -> a -> Doc ann
+  default gist'
+    :: (ConfigFor a ~ (), Pretty a) => Config -> a -> Doc ann
+  gist' conf = pretty
+   -- This clause avoids a "redundant constraint" warning, but shouldn't affect
+   -- semantics (including laziness):
+   where _x :: () = configLookup @a conf
 
-  gist' :: GistParams a -> a -> Doc ann
-  default gist' :: (GistParams a ~ (), Pretty a) => GistParams a -> a -> Doc ann
-  gist' ~() = pretty
-  -- ~() here doesn't force, but does avoid a redundant constraint warning
-
-  paramParser :: ParamParser (GistParams a)
-  paramParser = mempty
-
-  gist :: a -> Doc ann
-  gist = gist' mempty
-
-  gistP :: HasCallStack => [(String, String)] -> a -> Doc ann
-  gistP args = gist' $ either error id (runParamParser (paramParser @a) args)
-
-  -- | The instance for `Gist [a]` delegates to this. Customize it if you want
-  -- to customize that instance. Needed for `Gist String`.
-  type GistParamsList a :: Type
-  type GistParamsList a = (Last Int, GistParams a)
-
-  -- | The instance for `Gist [a]` delegates to this. Customize it if you want
-  -- to customize that instance. Needed for `Gist String`.
-  gistList' :: GistParamsList a -> [a] -> Doc ann
+  gistList' :: Config -> [a] -> Doc ann
   default gistList'
-    :: GistParamsList a ~ (Last Int, GistParams a)
-    => GistParamsList a -> [a] -> Doc ann
-  gistList' (Last mTake, p) l =
-    let elems = case mTake of
-          Nothing -> map (gist' p) l
+    :: (ConfigForList a ~ (Last Int, Last Config))
+    => Config
+    -> [a]
+    -> Doc ann
+  gistList' conf l =
+    let (Last mTake, Last mSubConf) =
+          configLookup @[] conf <> configLookupList @a conf
+        subConf = fromMaybe conf mSubConf
+        elems = case mTake of
+          Nothing -> map (gist' subConf) l
           Just n  -> case splitAt n l of
-            (start, []   ) -> map (gist' p) start
-            (start, _ : _) -> map (gist' p) start ++ ["..."]
-    in  align $ list elems
+            (start, []   ) -> map (gist' subConf) start
+            (start, _ : _) -> map (gist' subConf) start ++ ["..."]
+    in align $ list elems
 
-  -- | The instance for `Gist [a]` delegates to this. Customize it if you want
-  -- to customize that instance. Needed for `Gist String`.
-  paramParserList :: ParamParser (GistParamsList a)
-  default paramParserList
-    :: GistParamsList a ~ (Last Int, GistParams a)
-    => ParamParser (GistParamsList a)
-  paramParserList =
-    ((mempty, ) <$> paramParser @a) <> ((, mempty) <$> listParser)
+-- | This describes the type of the configuration for a gist.
+--
+-- Some things are `Configurable` but not `Gist`. In particular, types of kind
+-- other than `Type` can be `Configurable`, and gists can look up the config for
+-- those types too. So we can have one configuration for `Floating` and a more
+-- specific one for `Double`; or one for `[]` and a more specific one for
+-- `[()]`.
+class
+  ( Typeable a
+  , Monoid (ConfigFor a)
+  , Typeable (ConfigFor a)
+  , Monoid (ConfigForList a)
+  , Typeable (ConfigForList a)
+  ) => Configurable a
+ where
+  type ConfigFor a :: Type
+  type ConfigFor a = ()
+
+  parseConfigFor :: String -> Either String (ConfigFor a)
+  default parseConfigFor
+    :: ConfigFor a ~ () => String -> Either String (ConfigFor a)
+  parseConfigFor _ = Left "Not configurable"
+
+  type ConfigForList a :: Type
+  type ConfigForList a = ConfigFor []
+
+  parseConfigForList :: String -> Either String (ConfigForList a)
+
+  -- If we replace this constrant with `ConfigForList a ~ ConfigFor []`, we get
+  -- compile failures for some reason.
+  default parseConfigForList
+    :: ConfigForList a ~ (Last Int, Last Config)
+    => String
+    -> Either String (ConfigForList a)
+  parseConfigForList = parseConfigFor @[]
+
+data SomeConfigurable = forall a . Configurable a => SomeConfigurable
+                                                       !(TypeRep a)
+
+instance Eq SomeConfigurable where
+  SomeConfigurable a == SomeConfigurable b = SomeTypeRep a == SomeTypeRep b
+
+instance Ord SomeConfigurable where
+  SomeConfigurable a `compare` SomeConfigurable b =
+    SomeTypeRep a `compare` SomeTypeRep b
+
+newtype Config = Config (Map SomeConfigurable Dyn.Dynamic)
+
+instance Semigroup Config where
+  Config m1 <> Config m2 = Config $ Map.unionWithKey f m1 m2
    where
-    listParser = ParamParser $ Map.singleton "[]" $ \s -> case words s of
-      ["show-first", n] -> case readMaybe n of
-        Just n' -> Right (pure n')
-        Nothing -> Left "Expected \"show-first (int)\""
-      _ -> Left "Expected \"show-first (int)\""
+    f sc dyn1 dyn2 = case sc of
+      SomeConfigurable c -> concatDyns c dyn1 dyn2
 
-newtype ParamParser a = ParamParser (Map String (String -> Either String a))
-  deriving stock Functor
+    concatDyns
+      :: forall a
+       . Configurable a
+      => TypeRep a
+      -> Dyn.Dynamic
+      -> Dyn.Dynamic
+      -> Dyn.Dynamic
+    concatDyns _ dyn1 dyn2 =
+      case
+          ( Dyn.fromDynamic @(ConfigFor a) dyn1
+          , Dyn.fromDynamic @(ConfigFor a) dyn2
+          )
+        of
+          (Just c1, Just c2) -> Dyn.toDyn $ c1 <> c2
+          _                  -> error "Bad Dynamic saved in Config"
 
-instance Semigroup a => Semigroup (ParamParser a) where
-  ParamParser m1 <> ParamParser m2 = ParamParser $ Map.unionWith combine m1 m2
-    where f1 `combine` f2 = \s -> (<>) <$> f1 s <*> f2 s
+instance Monoid Config where
+  mempty = Config mempty
 
-instance Monoid a => Monoid (ParamParser a) where
-  mempty = ParamParser mempty
+configInsert :: forall a . Configurable a => ConfigFor a -> Config -> Config
+configInsert confFor (Config m) =
+  Config $ Map.insert (SomeConfigurable $ typeRep @a) (Dyn.toDyn confFor) m
 
-runParamParser
-  :: Monoid a => ParamParser a -> [(String, String)] -> Either String a
-runParamParser (ParamParser parser) args =
-  second mconcat $ for args $ \(ctx, p) -> if ctx == "*"
-    then Right $ mconcat $ Map.elems $ parser <&> \func ->
-      either mempty id (func p)
-    else case Map.lookup ctx parser of
-      Nothing   -> Right mempty
-      Just func -> func p
+configLookup :: forall a . Configurable a => Config -> ConfigFor a
+configLookup (Config m) = case Map.lookup (SomeConfigurable $ typeRep @a) m of
+  Nothing  -> mempty
+  Just dyn -> case Dyn.fromDynamic dyn of
+    Nothing   -> error "Bad Dynamic saved in Config"
+    Just conf -> conf
 
-instance Gist () where
-  type GistParams () = ()
+configLookupList :: forall a . Configurable a => Config -> ConfigForList a
+configLookupList (Config m) =
+  case Map.lookup (SomeConfigurable $ typeRep @a) m of
+    Nothing  -> mempty
+    Just dyn -> case Dyn.fromDynamic dyn of
+      Nothing   -> error "Bad Dynamic saved in Config"
+      Just conf -> conf
 
-instance Gist Int where
-  type GistParams Int = ()
+-- | Concatenates several configs into one before applying.
+--
+-- This lets us do `gist [strConfig ..., config ...]`. But maybe we want an
+-- `IsConfig` typeclass.
+gist :: Gist a => [Config] -> a -> Doc ann
+gist confs = gist' (mconcat confs)
 
-instance Gist Double where
-  type GistParams Double = Last Printf.FieldFormat
-  gist' = \case
+-- | Parse a `Config` from a string.
+strConfig :: forall a . (HasCallStack, Configurable a) => String -> Config
+strConfig s = case parseConfigFor @a s of
+  Left  err     -> error $ "Could not parse config: " <> err
+  Right confFor -> config @a confFor
+
+-- | Create a singleton `Config`.
+config :: forall a . Configurable a => ConfigFor a -> Config
+config confFor = configInsert @a confFor mempty
+
+instance Gist a => Gist [a] where
+  gist' = gistList' @a
+
+instance Configurable a => Configurable [a] where
+  type ConfigFor [a] = ConfigForList a
+  parseConfigFor = parseConfigForList @a
+
+instance Configurable [] where
+  type ConfigFor [] = (Last Int, Last Config)
+  parseConfigFor s = case words s of
+    ["show-first", n] -> case readMaybe n of
+      Just n' -> Right (pure n', mempty)
+      Nothing -> Left "Expected \"show-first (int)\""
+    _ -> Left "Expected \"show-first (int)\""
+
+instance Gist ()
+instance Configurable ()
+
+instance Gist Int
+instance Configurable Int
+
+instance Gist Float where
+  gist' conf = case configLookup @Floating conf <> configLookup @Float conf of
     Last Nothing  -> pretty
     Last (Just f) -> \d -> pretty $ Printf.formatRealFloat d f ""
 
-  paramParser = ParamParser $ Map.fromList [("Double", go), ("Floating", go)]
+instance Configurable Float where
+  type ConfigFor Float = ConfigFor Floating
+  parseConfigFor = parseConfigFor @Floating
+
+instance Gist Double where
+  gist' conf = case configLookup @Floating conf <> configLookup @Double conf of
+    Last Nothing  -> pretty
+    Last (Just f) -> \d -> pretty $ Printf.formatRealFloat d f ""
+
+instance Configurable Double where
+  type ConfigFor Double = ConfigFor Floating
+  parseConfigFor = parseConfigFor @Floating
+
+instance Configurable Floating where
+  type ConfigFor Floating = Last Printf.FieldFormat
+  parseConfigFor = go
    where
-    -- This is pretty awkward. Text.Printf doesn't expose any way to parse a
-    -- FieldFormat, so we do it ourselves. We might want our own version of
-    -- FieldFormat, to get a Show instance and to allow comma separation.
     go = \case
       [] -> Left "incomplete format string"
       ('-' : s) ->
@@ -151,13 +248,104 @@ instance Gist Double where
 
     a <&&> f = fmap (fmap f) a
 
-data GPStrQuotes
-  = GPStrQuotesAlways
-  | GPStrQuotesNever
-  | GPStrQuotesSometimes
+instance Configurable IsString where
+  type ConfigFor IsString = Last ConfStrQuotes
+  parseConfigFor = \case
+    "QuotesAlways"    -> Right $ pure ConfStrQuotesAlways
+    "QuotesNever"     -> Right $ pure ConfStrQuotesNever
+    "QuotesSometimes" -> Right $ pure ConfStrQuotesSometimes
+    _                 -> Left "unknown quote specifier"
+
+instance Gist Char where
+  gist' conf c =
+    case
+        fromLast ConfStrQuotesSometimes
+        $  configLookup @IsString conf
+        <> configLookup @Char conf
+      of
+        ConfStrQuotesAlways -> viaShow c
+        ConfStrQuotesNever  -> pretty c
+        ConfStrQuotesSometimes ->
+          if charWantsQuotes c then viaShow c else pretty c
+
+  gistList' conf s =
+    case
+        fromLast ConfStrQuotesSometimes
+        $  configLookup @IsString conf
+        <> configLookup @String conf
+      of
+        ConfStrQuotesAlways -> viaShow s
+        ConfStrQuotesNever  -> pretty s
+        ConfStrQuotesSometimes ->
+          if any charWantsQuotes s then viaShow s else pretty s
+
+instance Configurable Char where
+  type ConfigFor Char = ConfigFor IsString
+  parseConfigFor = parseConfigFor @IsString
+
+  type ConfigForList Char = Last ConfStrQuotes
+  parseConfigForList = parseConfigFor @Char
+
+instance Gist Text where
+  gist' conf s =
+    let myConf = configLookup @IsString conf <> configLookup @Text conf
+    in  gist' (configInsert @String myConf conf) (Text.unpack s)
+
+instance Configurable Text where
+  type ConfigFor Text = ConfigFor String
+  parseConfigFor = parseConfigFor @String
+
+instance (Gist a, Gist b) => Gist (a, b) where
+  gist' conf (a, b) = tupled
+    [gist' (fromLast conf confA) a, gist' (fromLast conf confB) b]
+   where
+    (confA, confB) =
+      configLookup @(,) conf
+        <> configLookup @((,) a) conf
+        <> configLookup @(a, b) conf
+
+instance (Typeable a, Typeable b) => Configurable (a, b) where
+  type ConfigFor (a, b) = (Last Config, Last Config)
+  parseConfigFor _ = Left "Cannot parse config for (a, b)"
+
+instance Configurable (,) where
+  type ConfigFor (,) = (Last Config, Last Config)
+  parseConfigFor _ = Left "Cannot parse config for (,)"
+
+instance Typeable a => Configurable ((,) a) where
+  type ConfigFor ((,) a) = (Last Config, Last Config)
+  parseConfigFor _ = Left "Cannot parse config for ((,) a)"
+
+instance (Gist k, Gist v) => Gist (Map k v) where
+  gist' conf m =
+    let
+      (ConfMap showKeys showVals, confK, confV) = configLookup @(Map k v) conf
+      showKV (k, v) =
+        (if fromLast True showKeys then gist' (fromLast conf confK) k else "_")
+          <> ": "
+          <> (if fromLast True showVals
+               then gist' (fromLast conf confV) v
+               else "_"
+             )
+    in
+      group
+      $ encloseSep (flatAlt "{ " "{") (flatAlt " }" "}") ", "
+      $ (showKV <$> Map.toList m)
+
+instance (Typeable k, Typeable v) => Configurable (Map k v) where
+  type ConfigFor (Map k v) = (ConfMap, Last Config, Last Config)
+  parseConfigFor = \case
+    "hide-keys" -> Right (ConfMap (pure False) mempty, mempty, mempty)
+    "hide-vals" -> Right (ConfMap mempty (pure False), mempty, mempty)
+    _           -> Left "Expected hide-keys or hide-vals"
+
+data ConfStrQuotes
+  = ConfStrQuotesAlways
+  | ConfStrQuotesNever
+  | ConfStrQuotesSometimes
   deriving stock (Eq, Show)
 
--- | If we use `GPStrQuotesSometimes`, this decides whether an individual
+-- | If we use `ConfStrQuotesSometimes`, this decides whether an individual
 -- character should be quoted. A string is quoted if it contains any characters
 -- that should be quoted.
 --
@@ -171,80 +359,16 @@ charWantsQuotes c =
        `notElem` [Char.ConnectorPunctuation, Char.DashPunctuation]
        )
 
-paramParserGPStrQuotes :: String -> ParamParser (Last GPStrQuotes)
-paramParserGPStrQuotes otherName = ParamParser
-  $ Map.fromList [("IsString", go), (otherName, go)]
- where
-  go = \case
-    "QuotesAlways"    -> Right $ pure GPStrQuotesAlways
-    "QuotesNever"     -> Right $ pure GPStrQuotesNever
-    "QuotesSometimes" -> Right $ pure GPStrQuotesSometimes
-    _                 -> Left "unknown quote specifier"
-
-instance Gist Char where
-  type GistParams Char = Last GPStrQuotes
-  gist' q c = case fromLast GPStrQuotesSometimes q of
-    GPStrQuotesAlways    -> viaShow c
-    GPStrQuotesNever     -> pretty c
-    GPStrQuotesSometimes -> if charWantsQuotes c then viaShow c else pretty c
-  paramParser = paramParserGPStrQuotes "Char"
-
-  type GistParamsList Char = Last GPStrQuotes
-  gistList' q s = gist' q (Text.pack s)
-  paramParserList = paramParserGPStrQuotes "String"
-
-instance Gist Text where
-  type GistParams Text = Last GPStrQuotes
-  gist' q = case fromLast GPStrQuotesSometimes q of
-    GPStrQuotesAlways -> viaShow
-    GPStrQuotesNever  -> pretty
-    GPStrQuotesSometimes ->
-      \t -> if Text.any charWantsQuotes t then viaShow t else pretty t
-  paramParser = paramParserGPStrQuotes "Text"
-
-instance (Gist a, Gist b) => Gist (a, b) where
-  type GistParams (a, b) = (GistParams a, GistParams b)
-  gist' (pa, pb) (a, b) = tupled [gist' pa a, gist' pb b]
-  paramParser =
-    ((, mempty) <$> paramParser @a) <> ((mempty, ) <$> paramParser @b)
-
-instance Gist a => Gist [a] where
-  type GistParams [a] = GistParamsList a
-  gist'       = gistList'
-  paramParser = paramParserList @a
-
-data GPMap = GPMap
-  { gpMapKeys :: Last Bool
-  , gpMapVals :: Last Bool
+data ConfMap = ConfMap
+  { confMapShowKeys :: Last Bool
+  , confMapShowVals :: Last Bool
   }
   deriving stock (Eq, Show)
 
-instance Semigroup GPMap where
-  (GPMap a1 b1) <> (GPMap a2 b2) = GPMap (a1 <> a2) (b1 <> b2)
-instance Monoid GPMap where
-  mempty = GPMap mempty mempty
-
-instance (Gist k, Gist v) => Gist (Map k v) where
-  type GistParams (Map k v) = (GPMap, GistParams k, GistParams v)
-  gist' (GPMap showKeys showVals, pk, pv) m =
-    group
-      $ encloseSep (flatAlt "{ " "{") (flatAlt " }" "}") ", "
-      $ (showKV <$> Map.toList m)
-   where
-    showKV (k, v) =
-      (if fromLast True showKeys then gist' pk k else "_")
-        <> ": "
-        <> (if fromLast True showVals then gist' pv v else "_")
-
-  paramParser =
-    ((mempty, , mempty) <$> paramParser @k)
-      <> ((mempty, mempty, ) <$> paramParser @v)
-      <> ((, mempty, mempty) <$> mapParser)
-   where
-    mapParser = ParamParser $ Map.singleton "Map" $ \case
-      "hide-keys" -> Right $ GPMap (pure False) mempty
-      "hide-vals" -> Right $ GPMap mempty (pure False)
-      _           -> Left "Expected hide-keys or hide-vals"
+instance Semigroup ConfMap where
+  (ConfMap a1 b1) <> (ConfMap a2 b2) = ConfMap (a1 <> a2) (b1 <> b2)
+instance Monoid ConfMap where
+  mempty = ConfMap mempty mempty
 
 fromLast :: a -> Last a -> a
 fromLast def = \case
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import           Data.Foldable                  ( for_ )
+import           Data.String                    ( IsString )
+import           Data.Text                      ( Text )
 import qualified Prettyprinter                 as PP
 import qualified Prettyprinter.Render.Text     as PPText
 import qualified Test.Hspec                    as Hspec
@@ -10,7 +12,8 @@ import           Test.Hspec                     ( describe
 import           Test.Hspec.Expectations
 import qualified Test.QuickCheck               as QC
 
-import           Gist                           ( gistP )
+import qualified Gist
+import           Gist                           ( gist )
 
 main :: IO ()
 main = Hspec.hspec $ do
@@ -18,37 +21,82 @@ main = Hspec.hspec $ do
     layout n = PPText.renderStrict . PP.layoutPretty
       (PP.defaultLayoutOptions { PP.layoutPageWidth = PP.AvailablePerLine n 1 })
 
-  describe "gisting Double" $ do
-    let tests =
+  describe "gisting floaty things" $ do
+    let tests :: Floating a => [([String], a, Text)]
+        tests =
           [ ([]       , 123.4, "123.4")
           , ([".3f"]  , 123.4, "123.400")
           , (["7.3f"] , 123.4, "123.400")
           , (["8.3f"] , 123.4, " 123.400")
           , (["08.3f"], 123.4, "0123.400")
           ]
-    for_ tests $ \(args, input :: Double, expected) -> do
-      let desc = concat [show args, " / ", show input, " === ", show expected]
-      it desc $ do
-        QC.forAll
-            ((,) <$> QC.arbitrary <*> QC.elements ["Double", "Floating", "*"])
-          $ \(width, context) ->
-              layout width (gistP ((context, ) <$> args) input)
-                `shouldBe` expected
 
-  describe "gisting String" $ do
-    let tests =
+    describe "Double" $ do
+      for_ tests $ \(args, input :: Double, expected) -> do
+        let desc =
+              concat [show args, " / ", show input, " === ", show expected]
+        it desc $ do
+          QC.forAll
+              ((,) <$> QC.arbitrary <*> QC.elements
+                [ ("Double"  , QC.Blind $ Gist.strConfig @Double)
+                , ("Floating", QC.Blind $ Gist.strConfig @Floating)
+                ]
+              )
+            $ \(width, (_ :: String, QC.Blind context)) ->
+                layout width (gist (context <$> args) input) `shouldBe` expected
+
+    describe "Float" $ do
+      for_ tests $ \(args, input :: Float, expected) -> do
+        let desc =
+              concat [show args, " / ", show input, " === ", show expected]
+        it desc $ do
+          QC.forAll
+              ((,) <$> QC.arbitrary <*> QC.elements
+                [ ("Float"   , QC.Blind $ Gist.strConfig @Float)
+                , ("Floating", QC.Blind $ Gist.strConfig @Floating)
+                ]
+              )
+            $ \(width, (_ :: String, QC.Blind context)) ->
+                layout width (gist (context <$> args) input) `shouldBe` expected
+
+  describe "gisting stringy things" $ do
+    let tests :: IsString a => [([String], a, Text)]
+        tests =
           [ (["QuotesAlways"]   , "foo"    , "\"foo\"")
           , (["QuotesSometimes"], "foo"    , "foo")
           , (["QuotesNever"]    , "foo"    , "foo")
+          , (["QuotesAlways"]   , "foo-bar", "\"foo-bar\"")
+          , (["QuotesSometimes"], "foo-bar", "foo-bar")
+          , (["QuotesNever"]    , "foo-bar", "foo-bar")
           , (["QuotesAlways"]   , "foo bar", "\"foo bar\"")
           , (["QuotesSometimes"], "foo bar", "\"foo bar\"")
           , (["QuotesNever"]    , "foo bar", "foo bar")
           ]
-    for_ tests $ \(args, input :: String, expected) -> do
-      let desc = concat [show args, " / ", show input, " === ", show expected]
-      it desc $ do
-        QC.forAll
-            ((,) <$> QC.arbitrary <*> QC.elements ["IsString", "String", "*"])
-          $ \(width, context) ->
-              layout width (gistP ((context, ) <$> args) input)
-                `shouldBe` expected
+
+    describe "String" $ do
+      for_ tests $ \(args, input :: String, expected) -> do
+        let desc =
+              concat [show args, " / ", show input, " === ", show expected]
+        it desc $ do
+          QC.forAll
+              ((,) <$> QC.arbitrary <*> QC.elements
+                [ ("IsString", QC.Blind $ Gist.strConfig @IsString)
+                , ("String"  , QC.Blind $ Gist.strConfig @String)
+                ]
+              )
+            $ \(width, (_ :: String, QC.Blind context)) ->
+                layout width (gist (context <$> args) input) `shouldBe` expected
+
+    describe "Text" $ do
+      for_ tests $ \(args, input :: Text, expected) -> do
+        let desc =
+              concat [show args, " / ", show input, " === ", show expected]
+        it desc $ do
+          QC.forAll
+              ((,) <$> QC.arbitrary <*> QC.elements
+                [ ("IsString", QC.Blind $ Gist.strConfig @IsString)
+                , ("String"  , QC.Blind $ Gist.strConfig @Text)
+                ]
+              )
+            $ \(width, (_ :: String, QC.Blind context)) ->
+                layout width (gist (context <$> args) input) `shouldBe` expected
