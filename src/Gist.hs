bd09ae4ecfd24bc5c64e78eb2d0268482fa43ac8
Philip Hazelden <philip.hazelden@gmail.com>
2023-03-19 23:36:24 +0000

    Add a separate Config type inside Gist.
    Makes configuration more transparent, but more awkward to use the
    library. Not actually convinced this is an improvement. Gonna revert.

--- src/Gist.hs
+++ src/Gist.hs
@@ -13,6 +13,7 @@ module Gist
 import           Data.Bifunctor                 ( first )
 import qualified Data.Char                     as Char
 import qualified Data.Dynamic                  as Dyn
+import           Data.Functor.Identity          ( Identity(..) )
 import           Data.Kind                      ( Type )
 import qualified Data.Map.Strict               as Map
 import           Data.Map.Strict                ( Map )
@@ -32,30 +33,46 @@ import           Type.Reflection                ( SomeTypeRep(..)
                                                 )
 
 -- | A gist is a configurable pretty-print.
-class Configurable a => Gist a where
-  {-# MINIMAL gist' | gistPrec' #-}
+class Typeable a => Gist a where
+  {-# MINIMAL configFor, (gist' | gistPrec') #-}
 
-  gistPrec' :: Int -> Config -> a -> Doc ann
+  type ConfigFor a :: Type
+  configFor :: Config -> ConfigFor a
+
+  gistPrec' :: Int -> ConfigFor a -> a -> Doc ann
   gistPrec' _ = gist'
 
-  gist' :: Config -> a -> Doc ann
+  gist' :: ConfigFor a -> a -> Doc ann
   gist' = gistPrec' 0
 
-  gistList' :: Config -> [a] -> Doc ann
-  default gistList'
-    :: (ConfigForList a ~ (Last Int, Last Config))
+  type ConfigForList a :: Type
+  type ConfigForList a = (Maybe Int, Config)
+
+  configForList :: Config -> ConfigForList a
+  default configForList
+    :: ( Configurable a
+       , ConfigForList a ~ (Maybe Int, Config)
+       , WriteConfigForList a ~ (Last Int, Last Config)
+       )
     => Config
+    -> ConfigForList a
+  configForList conf =
+    let (Last mTake, Last mSubConf) = configLookups @( [] :&& [a] ) conf
+    in (mTake, fromMaybe conf mSubConf)
+
+  gistList' :: ConfigForList a -> [a] -> Doc ann
+  default gistList'
+    :: (ConfigForList a ~ (Maybe Int, Config))
+    => ConfigForList a
     -> [a]
     -> Doc ann
-  gistList' conf l =
-    let (Last mTake, Last mSubConf) =
-          configLookups @( [] :&& [a] ) conf
-        subConf = fromMaybe conf mSubConf
+  gistList' (mTake, subConf) l =
+    let subConfFor = configFor @a subConf
         elems = case mTake of
-          Nothing -> map (gist' subConf) l
+          Nothing -> map (gist' subConfFor) l
           Just n  -> case splitAt n l of
-            (start, []   ) -> map (gist' subConf) start
-            (start, _ : _) -> map (gist' subConf) start ++ ["..."]
+            (start, []   ) -> map (gist' subConfFor) start
+            (start, _ : _) -> map (gist' subConfFor) start ++ ["..."]
     in align $ list elems
 
 -- | This describes the type of the configuration for a gist.
@@ -67,31 +84,34 @@ class Configurable a => Gist a where
 -- `[()]`.
 class
   ( Typeable a
-  , Monoid (ConfigFor a)
-  , Typeable (ConfigFor a)
-  , Monoid (ConfigForList a)
-  , Typeable (ConfigForList a)
+  , Monoid (WriteConfigFor a)
+  , Typeable (WriteConfigFor a)
+  , Monoid (WriteConfigForList a)
+  , Typeable (WriteConfigForList a)
   ) => Configurable a
  where
-  type ConfigFor a :: Type
-  type ConfigFor a = ()
+  type WriteConfigFor a :: Type
+  type WriteConfigFor a = ()
 
-  parseConfigFor :: String -> Either String (ConfigFor a)
+  parseConfigFor :: String -> Either String (WriteConfigFor a)
   default parseConfigFor
-    :: ConfigFor a ~ () => String -> Either String (ConfigFor a)
+    :: WriteConfigFor a ~ () => String -> Either String (WriteConfigFor a)
   parseConfigFor _ = Left "Not configurable"
 
-  type ConfigForList a :: Type
-  type ConfigForList a = ConfigFor []
+  type WriteConfigForList a :: Type
+  type WriteConfigForList a = WriteConfigFor []
 
-  parseConfigForList :: String -> Either String (ConfigForList a)
+  parseConfigForList :: String -> Either String (WriteConfigForList a)
 
-  -- If we replace this constrant with `ConfigForList a ~ ConfigFor []`, we get
-  -- compile failures for some reason.
+  -- If we replace this constrant with
+  --
+  --     WriteConfigForList a ~ WriteConfigFor
+  --
+  -- we get compile failures for some reason.
   default parseConfigForList
-    :: ConfigForList a ~ (Last Int, Last Config)
+    :: WriteConfigForList a ~ (Last Int, Last Config)
     => String
-    -> Either String (ConfigForList a)
+    -> Either String (WriteConfigForList a)
   parseConfigForList = parseConfigFor @[]
 
 data SomeConfigurable = forall a . Configurable a => SomeConfigurable
@@ -125,8 +145,8 @@ instance Semigroup Config where
       -> Dyn.Dynamic
     concatDyns _ dyn1 dyn2 =
       case
-          ( Dyn.fromDynamic @(ConfigFor a) dyn1
-          , Dyn.fromDynamic @(ConfigFor a) dyn2
+          ( Dyn.fromDynamic @(WriteConfigFor a) dyn1
+          , Dyn.fromDynamic @(WriteConfigFor a) dyn2
           )
         of
           (Just c1, Just c2) -> Dyn.toDyn $ c1 <> c2
@@ -135,11 +155,12 @@ instance Semigroup Config where
 instance Monoid Config where
   mempty = UnsafeConfig mempty
 
-configInsert :: forall a . Configurable a => ConfigFor a -> Config -> Config
+configInsert
+  :: forall a . Configurable a => WriteConfigFor a -> Config -> Config
 configInsert confFor (UnsafeConfig m) = UnsafeConfig
   $ Map.insert (SomeConfigurable $ typeRep @a) (Dyn.toDyn confFor) m
 
-configLookup :: forall a . Configurable a => Config -> ConfigFor a
+configLookup :: forall a . Configurable a => Config -> WriteConfigFor a
 configLookup (UnsafeConfig m) =
   case Map.lookup (SomeConfigurable $ typeRep @a) m of
     Nothing  -> mempty
@@ -161,7 +182,7 @@ data (:&&) a b -- brittany doesn't like `a :&& b`.
 infix 6 :&&
 
 -- | We can use this class to lookup configs for multiple types at once,
--- provided they all have the same `ConfigFor`. Replace
+-- provided they all have the same `WriteConfigFor`. Replace
 --
 --     configLookup @a conf
 --       <> configLookup @b conf
@@ -175,19 +196,19 @@ class ConfigLookups as where
   type ConfigLookupsResult as
   configLookups :: Config -> ConfigLookupsResult as
 
-instance (Configurable a, Configurable b, ConfigFor a ~ ConfigFor b)
+instance (Configurable a, Configurable b, WriteConfigFor a ~ WriteConfigFor b)
   => ConfigLookups (a :&& b)
  where
-  type ConfigLookupsResult (a :&& b) = ConfigFor a
+  type ConfigLookupsResult (a :&& b) = WriteConfigFor a
   configLookups conf = configLookup @a conf <> configLookup @b conf
 
 instance
   ( Configurable a
   , ConfigLookups as
-  , ConfigFor a ~ ConfigLookupsResult as
+  , WriteConfigFor a ~ ConfigLookupsResult as
   ) => ConfigLookups (as :& a)
  where
-  type ConfigLookupsResult (as :& a) = ConfigFor a
+  type ConfigLookupsResult (as :& a) = WriteConfigFor a
   configLookups conf = configLookups @as conf <> configLookup @a conf
 
 -- | For types with a `Pretty` instance, you can derive `Gist` and
@@ -195,11 +216,10 @@ instance
 -- configuration.
 newtype Prettily a = Prettily a
 
-instance (Configurable a, Pretty a, ConfigFor a ~ ()) => Gist (Prettily a) where
-  gistPrec' _ conf (Prettily a) = pretty a
-    -- This clause avoids a "redundant constraint" warning, but shouldn't affect
-    -- semantics (including laziness):
-    where _x :: () = configLookup @a conf
+instance (Configurable a, Pretty a, WriteConfigFor a ~ ()) => Gist (Prettily a) where
+  type ConfigFor (Prettily a) = ()
+  configFor _ = ()
+  gistPrec' _ ~() (Prettily a) = pretty a
 
 instance Typeable a => Configurable (Prettily a)
 
@@ -207,8 +227,8 @@ instance Typeable a => Configurable (Prettily a)
 --
 -- This lets us do `gist [strConfig ..., config ...]`. But maybe we want an
 -- `IsConfig` typeclass.
-gist :: Gist a => [Config] -> a -> Doc ann
-gist confs = gist' (mconcat confs)
+gist :: forall a ann . Gist a => [Config] -> a -> Doc ann
+gist confs = gist' $ configFor @a $ mconcat confs
 
 -- | Parse a `Config` from a string.
 strConfig :: forall a . (HasCallStack, Configurable a) => String -> Config
@@ -217,18 +237,32 @@ strConfig s = case parseConfigFor @a s of
   Right confFor -> config @a confFor
 
 -- | Create a singleton `Config`.
-config :: forall a . Configurable a => ConfigFor a -> Config
+config :: forall a . Configurable a => WriteConfigFor a -> Config
 config confFor = configInsert @a confFor mempty
 
-instance Gist a => Gist [a] where
+-- Most `Gist` instances don't explicitly need a `Configurable` instance, but
+-- this one does. e.g. we have
+--
+--     instance (Gist k, Gist v) => Gist (Map k v)
+--     instance (Configurable a, Gist a) => Gist [a]
+--
+-- The difference is that the `Gist (Map k v)` instance does need `Configurable
+-- (Map k v)`, but that only depends on `(Typeable k, Typeable v)`, which are
+-- provided by `(Gist k, Gist v)`. Meanwhile the `Configurable [a]` instance
+-- depends on `Configurable a`, because of `WriteConfigForList` and friends. So
+-- we can't deduce `Configurable [a]` simply from `Gist a`, whereas we can
+-- deduce `Configurable (Map k v)` from `(Gist k, Gist v)`.
+instance (Configurable a, Gist a) => Gist [a] where
+  type ConfigFor [a] = ConfigForList a
+  configFor = configForList @a
   gistPrec' _ = gistList' @a
 
 instance Configurable a => Configurable [a] where
-  type ConfigFor [a] = ConfigForList a
+  type WriteConfigFor [a] = WriteConfigForList a
   parseConfigFor = parseConfigForList @a
 
 instance Configurable [] where
-  type ConfigFor [] = (Last Int, Last Config)
+  type WriteConfigFor [] = (Last Int, Last Config)
   parseConfigFor s = case words s of
     ["show-first", n] -> case readMaybe n of
       Just n' -> Right (pure n', mempty)
@@ -243,27 +277,31 @@ deriving via (Prettily Int) instance Gist Int
 deriving via (Prettily Int) instance Configurable Int
 
 instance Gist Float where
-  gistPrec' _ conf = case configLookups @(Floating :&& Float) conf of
-    Last Nothing  -> pretty
-    Last (Just f) -> \d -> pretty $ Printf.formatRealFloat d f ""
+  type ConfigFor Float = Maybe Printf.FieldFormat
+  configFor conf = getLast $ configLookups @(Floating :&& Float) conf
+  gistPrec' _ = \case
+    Nothing -> pretty
+    Just f  -> \d -> pretty $ Printf.formatRealFloat d f ""
 
 instance Configurable Float where
-  type ConfigFor Float = ConfigFor Floating
+  type WriteConfigFor Float = WriteConfigFor Floating
   parseConfigFor = parseConfigFor @Floating
 
 instance Gist Double where
-  gistPrec' _ conf = case configLookups @(Floating :&& Double) conf of
-    Last Nothing  -> pretty
-    Last (Just f) -> \d -> pretty $ Printf.formatRealFloat d f ""
+  type ConfigFor Double = Maybe Printf.FieldFormat
+  configFor conf = getLast $ configLookups @(Floating :&& Double) conf
+  gistPrec' _ = \case
+    Nothing -> pretty
+    Just f  -> \d -> pretty $ Printf.formatRealFloat d f ""
 
 instance Configurable Double where
-  type ConfigFor Double = ConfigFor Floating
+  type WriteConfigFor Double = WriteConfigFor Floating
   parseConfigFor = parseConfigFor @Floating
 
 -- | TODO: allow comma and underscore separation. Also, there's no way to revert
 -- to the default behavior. And there's no instance for `Show FieldFormat`.
 instance Configurable Floating where
-  type ConfigFor Floating = Last Printf.FieldFormat
+  type WriteConfigFor Floating = Last Printf.FieldFormat
   parseConfigFor = go
    where
     go = \case
@@ -328,7 +366,7 @@ charWantsQuotes c =
 
 -- | TODO: allow (default to?) C-style escaping or similar.
 instance Configurable IsString where
-  type ConfigFor IsString = Last ConfStrQuotes
+  type WriteConfigFor IsString = Last ConfStrQuotes
   parseConfigFor = \case
     "quotes-always"    -> Right $ pure ConfStrQuotesAlways
     "quotes-never"     -> Right $ pure ConfStrQuotesNever
@@ -336,117 +374,132 @@ instance Configurable IsString where
     _                  -> Left "unknown quote specifier"
 
 instance Gist Char where
-  gistPrec' _ conf c =
-    case
-        fromLast ConfStrQuotesSometimes
-          $ configLookups @(IsString :&& Char) conf
-      of
-        ConfStrQuotesAlways -> viaShow c
-        ConfStrQuotesNever  -> pretty c
-        ConfStrQuotesSometimes ->
-          if charWantsQuotes c then viaShow c else pretty c
+  type ConfigFor Char = ConfStrQuotes
+  configFor =
+    fromLast ConfStrQuotesSometimes . configLookups @(IsString :&& Char)
+  gistPrec' _ conf c = case conf of
+    ConfStrQuotesAlways    -> viaShow c
+    ConfStrQuotesNever     -> pretty c
+    ConfStrQuotesSometimes -> if charWantsQuotes c then viaShow c else pretty c
 
-  gistList' conf s =
-    case
-        fromLast ConfStrQuotesSometimes
-          $ configLookups @(IsString :&& String) conf
-      of
-        ConfStrQuotesAlways -> viaShow s
-        ConfStrQuotesNever  -> pretty s
-        ConfStrQuotesSometimes ->
-          if any charWantsQuotes s then viaShow s else pretty s
+  type ConfigForList Char = ConfStrQuotes
+  configForList =
+    fromLast ConfStrQuotesSometimes . configLookups @(IsString :&& String)
+  gistList' conf s = gist' @Text conf (Text.pack s)
 
 instance Configurable Char where
-  type ConfigFor Char = ConfigFor IsString
+  type WriteConfigFor Char = WriteConfigFor IsString
   parseConfigFor = parseConfigFor @IsString
 
-  type ConfigForList Char = Last ConfStrQuotes
+  type WriteConfigForList Char = Last ConfStrQuotes
   parseConfigForList = parseConfigFor @Char
 
 instance Gist Text where
-  gistPrec' _ conf s = gist' (configInsert @String myConf conf) (Text.unpack s)
-    where myConf = configLookups @(IsString :&& Text) conf
+  type ConfigFor Text = ConfStrQuotes
+  configFor =
+    fromLast ConfStrQuotesSometimes . configLookups @(IsString :&& Text)
+  gistPrec' _ conf s = case conf of
+    ConfStrQuotesAlways -> viaShow s
+    ConfStrQuotesNever  -> pretty s
+    ConfStrQuotesSometimes ->
+      if Text.any charWantsQuotes s then viaShow s else pretty s
 
 instance Configurable Text where
-  type ConfigFor Text = ConfigFor String
+  type WriteConfigFor Text = WriteConfigFor String
   parseConfigFor = parseConfigFor @String
 
 instance (Gist a, Gist b) => Gist (a, b) where
-  gistPrec' _ conf (a, b) = tupled
-    [gist' (fromLast conf confA) a, gist' (fromLast conf confB) b]
+  type ConfigFor (a, b) = (Config, Config)
+  configFor conf = (fromLast conf confA, fromLast conf confB)
     where (confA, confB) = configLookups @((,) :&& ((,) a) :& (a, b)) conf
+  gistPrec' _ (confA, confB) (a, b) =
+    tupled [gist' (configFor @a confA) a, gist' (configFor @b confB) b]
 
 instance (Typeable a, Typeable b) => Configurable (a, b) where
-  type ConfigFor (a, b) = (Last Config, Last Config)
+  type WriteConfigFor (a, b) = (Last Config, Last Config)
   parseConfigFor _ = Left "Cannot parse config for (a, b)"
 
 instance Configurable (,) where
-  type ConfigFor (,) = (Last Config, Last Config)
+  type WriteConfigFor (,) = (Last Config, Last Config)
   parseConfigFor _ = Left "Cannot parse config for (,)"
 
 instance Typeable a => Configurable ((,) a) where
-  type ConfigFor ((,) a) = (Last Config, Last Config)
+  type WriteConfigFor ((,) a) = (Last Config, Last Config)
   parseConfigFor _ = Left "Cannot parse config for ((,) a)"
 
-data ConfMap = ConfMap
-  { confMapShowKeys :: Last Bool
-  , confMapShowVals :: Last Bool
+data ConfMap f = ConfMap
+  { confMapShowKeys :: f Bool
+  , confMapShowVals :: f Bool
   }
-  deriving stock (Eq, Show)
 
-instance Semigroup ConfMap where
+deriving stock instance Eq (f Bool) => Eq (ConfMap f)
+deriving stock instance Show (f Bool) => Show (ConfMap f)
+
+instance Semigroup (f Bool) => Semigroup (ConfMap f) where
   (ConfMap a1 b1) <> (ConfMap a2 b2) = ConfMap (a1 <> a2) (b1 <> b2)
-instance Monoid ConfMap where
+
+instance Monoid (f Bool) => Monoid (ConfMap f) where
   mempty = ConfMap mempty mempty
 
 instance (Gist k, Gist v) => Gist (Map k v) where
-  gistPrec' _ conf m =
+  type ConfigFor (Map k v) = (ConfMap Identity, Config, Config)
+
+  configFor conf =
+    ( ConfMap (Identity $ fromLast True showKeys)
+              (Identity $ fromLast True showVals)
+    , fromLast conf confK
+    , fromLast conf confV
+    )
+   where
+    (ConfMap showKeys showVals, confK, confV) =
+      configLookups @(Map :&& Map k :& Map k v) conf
+
+  gistPrec' _ (ConfMap showKeys showVals, confK, confV) m =
     group
       $ encloseSep (flatAlt "{ " "{") (flatAlt " }" "}") ", "
       $ (showKV <$> Map.toList m)
    where
-    (ConfMap showKeys showVals, confK, confV) =
-      configLookups @(Map :&& Map k :& Map k v) conf
     showKV (k, v) =
-      (if fromLast True showKeys then gist' (fromLast conf confK) k else "_")
+      (if runIdentity showKeys then gist' (configFor @k confK) k else "_")
         <> ": "
-        <> (if fromLast True showVals
-             then gist' (fromLast conf confV) v
-             else "_"
-           )
+        <> (if runIdentity showVals then gist' (configFor @v confV) v else "_")
 
 instance (Typeable k, Typeable v) => Configurable (Map k v) where
-  type ConfigFor (Map k v) = ConfigFor Map
+  type WriteConfigFor (Map k v) = WriteConfigFor Map
   parseConfigFor = parseConfigFor @Map
 
 instance Typeable k => Configurable (Map k) where
-  type ConfigFor (Map k) = ConfigFor Map
+  type WriteConfigFor (Map k) = WriteConfigFor Map
   parseConfigFor = parseConfigFor @Map
 
 instance Configurable Map where
-  type ConfigFor Map = (ConfMap, Last Config, Last Config)
+  type WriteConfigFor Map = (ConfMap Last, Last Config, Last Config)
   parseConfigFor = \case
     "hide-keys" -> Right (ConfMap (pure False) mempty, mempty, mempty)
     "hide-vals" -> Right (ConfMap mempty (pure False), mempty, mempty)
     _           -> Left "Expected hide-keys or hide-vals"
 
 instance (Gist a, Gist b) => Gist (Either a b) where
-  gistPrec' prec conf = parensIfPrecGT 10 prec . \case
-    Left  a -> "Left" <+> gistPrec' 11 (fromLast conf confL) a
-    Right a -> "Right" <+> gistPrec' 11 (fromLast conf confR) a
+  type ConfigFor (Either a b) = (Config, Config)
+
+  configFor conf = (fromLast conf confL, fromLast conf confR)
    where
     (confL, confR) = configLookups @(Either :&& Either a :& Either a b) conf
 
+  gistPrec' prec (confL, confR) = parensIfPrecGT 10 prec . \case
+    Left  a -> "Left" <+> gistPrec' 11 (configFor @a confL) a
+    Right a -> "Right" <+> gistPrec' 11 (configFor @b confR) a
+
 instance (Typeable a, Typeable b) => Configurable (Either a b) where
-  type ConfigFor (Either a b) = ConfigFor Either
+  type WriteConfigFor (Either a b) = WriteConfigFor Either
   parseConfigFor = parseConfigFor @Either
 
 instance Typeable a => Configurable (Either a) where
-  type ConfigFor (Either a) = ConfigFor Either
+  type WriteConfigFor (Either a) = WriteConfigFor Either
   parseConfigFor = parseConfigFor @Either
 
 instance Configurable Either where
-  type ConfigFor Either = (Last Config, Last Config)
+  type WriteConfigFor Either = (Last Config, Last Config)
   parseConfigFor _ = Left "Cannot parse config for Either"
 
 fromLast :: a -> Last a -> a
